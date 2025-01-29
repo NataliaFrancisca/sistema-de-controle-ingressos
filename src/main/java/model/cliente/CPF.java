f42d5131b277f4ff790a66541103a7c00eb708c1
nataliafrancisca <nathifrancisca@gmail.com>
2025-01-28 22:00:20 -0300

    feat: add the option to validate CPF using library

--- src/main/java/model/cliente/CPF.java
+++ src/main/java/model/cliente/CPF.java
@@ -1,5 +1,9 @@
 package model.cliente;
 
+import br.com.caelum.stella.ValidationMessage;
+import br.com.caelum.stella.validation.CPFValidator;
+
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,6 +13,14 @@ public class CPF {
         String regex = "^\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(cpf);
+
+
         return matcher.matches();
+    }
+
+    boolean validarCPFUsandoBiblioteca(String cpf){
+        CPFValidator cpfValidator = new CPFValidator();
+        List<ValidationMessage> errors = cpfValidator.invalidMessagesFor(cpf);
+        return errors.isEmpty();
     }
 }
